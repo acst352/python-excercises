7a30e0638c9ce1685f8164246e41a04351c33038
acst352 <acst352@gmail.com>
2023-10-03 17:38:57 -0600

    Update favouriteActor
    User can now enter the name of his favorite actor

--- Strings/favouriteActor.py
+++ Strings/favouriteActor.py
@@ -1,4 +1,3 @@
 # Make a program that displays your favourite actor/actress.
-favorite_actor_actress = "Mark Whalberg"
-message = "My favorite actor/actress is: " + favorite_actor_actress
-print(message)
+favorite_actor_actress = input("Enter the name of your favourite actor or actress:")
+print("Your favourite actor or actress is:", favorite_actor_actress)
