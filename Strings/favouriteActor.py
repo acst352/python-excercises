f3d1e0f576b8827e247d9e03d0fd25f3a749d61d
acst352 <acst352@gmail.com>
2023-10-03 17:52:24 -0600

    Update favoriteActor

--- Strings/favouriteActor.py
+++ Strings/favouriteActor.py
@@ -1,3 +1,3 @@
 # Make a program that displays your favourite actor/actress.
-favorite_actor_actress = input("Enter the name of your favourite actor or actress:")
-print("Your favourite actor or actress is:", favorite_actor_actress)
+favorite_actor_actress = input("Enter the name of your favorite actor or actress: ")
+print("Your favorite actor or actress is", favorite_actor_actress)
