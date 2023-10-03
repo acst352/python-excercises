daa05de224194dcf01c3c84ae7a397bd730b81b7
acst352 <acst352@gmail.com>
2023-10-03 17:04:42 -0600

    Create favouriteActor.py

---
+++ Strings/favouriteActor.py
@@ -0,0 +1,4 @@
+# Make a program that displays your favourite actor/actress.
+favorite_actor_actress = "Mark Whalberg"
+message = "My favorite actor/actress is: " + favorite_actor_actress
+print(message)
