888b6e358b7daf2743e03ce69a3a0d559d6139ac
acst352 <acst352@gmail.com>
2023-10-03 15:21:34 -0600

    Update displaySeveralNumbers

--- Variables/displaySeveralNumbers.py
+++ Variables/displaySeveralNumbers.py
@@ -1,1 +1,5 @@
-#Make a program that displays several numbers.
+# Make a program that displays several numbers.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers:
+    print(number)
