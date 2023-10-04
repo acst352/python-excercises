b133dcefaa4d80a0919729a57d13f9d92f26e97c
acst352 <acst352@gmail.com>
2023-10-03 19:56:00 -0600

    Create replaceMethodTwice.py

---
+++ String replace/replaceMethodTwice.py
@@ -0,0 +1,10 @@
+# Can a string be replaced twice?
+string = "A B C"
+modified_string = string.replace("A", "X")
+modified_string2 = string.replace("B", "Y")
+modified_string3 = string.replace("C", "Z")
+
+print(string)
+print(modified_string)
+print(modified_string2)
+print(modified_string3)
