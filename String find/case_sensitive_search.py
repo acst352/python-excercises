e4dad5d97251f66f18104aa6f84b39d08fe3868e
acst352 <acst352@gmail.com>
2023-10-05 23:46:00 -0600

    Create case_sensitive_search.py

---
+++ String find/case_sensitive_search.py
@@ -0,0 +1,6 @@
+text = "Hello, World! Hello, world!"
+index1 = text.find("Hello")  # Case-sensitive search
+index2 = text.find("hello")  # Case-sensitive search
+
+print("Index of 'Hello':", index1)  # Output: Index of 'Hello': 0
+print("Index of 'hello':", index2)  # Output: Index of 'hello': -1
