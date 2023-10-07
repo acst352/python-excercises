d26da14934ad14c1f8b4186b799f4ecbbeec29a9
acst352 <acst352@gmail.com>
2023-10-06 23:45:46 -0600

    Create 3_random_numbers.py

---
+++ Random numbers/3_random_numbers.py
@@ -0,0 +1,8 @@
+import random
+
+# Generate and print 3 random numbers
+for _ in range(3):
+    random_number = random.randint(
+        1, 100
+    )  # Generates a random integer between 1 and 100 (inclusive)
+    print(random_number)
