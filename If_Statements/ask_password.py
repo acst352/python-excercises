b64f431b1e2ad4fa5afbe1365624027721a4b5bf
acst352 <acst352@gmail.com>
2023-10-12 23:21:51 -0600

    Update ask password.py

--- If_Statements/ask_password.py
+++ If_Statements/ask_password.py
@@ -1,1 +1,10 @@
 # Make a program that asks a password.
+
+correct_password = "secretpassword"
+
+user_password = input("Enter the password: ")
+
+if user_password == correct_password:
+    print("Access granted. Welcome!")
+else:
+    print("Access denied. Incorrect password.")
