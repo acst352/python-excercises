e6897a1d42e30fde3d68c8dd92da11e84ed62988
acst352 <acst352@gmail.com>
2023-10-14 22:46:09 -0600

    For loops First commit

---
+++ For loops/For loops.py
@@ -0,0 +1,12 @@
+# Make a program that lists the countries in the set
+# clist = ['Canada','USA','Mexico','Australia']
+
+# Create a loop that counts from 0 to 100
+
+# Make a multiplication table using a loop
+
+# Output the numbers 1 to 10 backwards using a loop
+
+# Create a loop that counts all even numbers to 10
+
+# Create a loop that sums the numbers from 100 to 200
