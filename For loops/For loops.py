97918fc4ff071e5342c095601d712f73e4af5214
acst352 <acst352@gmail.com>
2023-10-19 23:48:00 -0600

    Create multiplication table

--- For loops/For loops.py
+++ For loops/For loops.py
@@ -10,6 +10,17 @@ for i in range(101):
 
 # Make a multiplication table using a loop
 
+num_rows = 10
+num_columns = 10
+
+for i in range(1, num_rows + 1):
+    for j in range(1, num_columns + 1):
+        result = i * j
+
+        print(f"{i} x {j} = {result:2}", end="\t")
+
+    print()
+
 # Output the numbers 1 to 10 backwards using a loop
 
 # Create a loop that counts all even numbers to 10
